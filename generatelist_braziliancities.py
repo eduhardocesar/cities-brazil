87db9818a916f72955dd3c16b960f0835517b276
Eduardo César <eduardocesar92@gmail.com>
2021-09-07 20:50:22 -0300

    Made some adjustments in the code for better performance

--- generatelist_braziliancities.py
+++ generatelist_braziliancities.py
@@ -1,112 +1,105 @@
-import requests
-import json
+import time
+import numpy as np
 import pandas as pd
 from pandas import json_normalize
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
 from tqdm import tqdm
 
-tqdm.pandas()
-nom = Nominatim(user_agent="eduhardocesar")
-geolocator = RateLimiter(nom.geocode, min_delay_seconds=1)
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
-r_ibge_states = requests.get(
-    "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
-)
-states = json_normalize(data=r_ibge_states.json(), sep="")
+import httpx
+import asyncio
+import nest_asyncio
+nest_asyncio.apply()
 
-base_url_pop = "https://agenciadenoticias.ibge.gov.br/media/com_mediaibge/arquivos/"
-arq_pop = base_url_pop + "de3c3890d5e127db41740496aa4ec20f.xls"
+ano_pop = '2021'
+arq_pop = "https://ftp.ibge.gov.br/Estimativas_de_Populacao/Estimativas_{}/estimativa_dou_{}.xls".format(
+    ano_pop, ano_pop)
 
 df_population = pd.read_excel(arq_pop, sheet_name="Municípios")
 df_population.columns = df_population.loc[0]
 df_population = df_population.drop(df_population.index[0])
 df_population = df_population[df_population["COD. UF"].notnull()]
-df_population.rename(
-    columns={"NOME DO MUNICÍPIO": "City", "POPULAÇÃO ESTIMADA": "Population"},
-    inplace=True,
-)
-df_population["ID_IBGE"] = (
-    df_population["COD. UF"].map(str).str[:2]
-    + df_population["COD. MUNIC"].map(str).str[:5]
-)
-df_population["Population"] = (
-    df_population["Population"]
-    .str.split(" ")
-    .str[0]
-    .fillna(df_population["Population"])
-)
+df_population.rename(columns={"NOME DO MUNICÍPIO": "City",
+                     "POPULAÇÃO ESTIMADA": "Population"}, inplace=True,)
+df_population["ID_IBGE"] = (df_population["COD. UF"].map(
+    str).str[:2] + df_population["COD. MUNIC"].map(str).str[:5]).astype(str).astype(int)
+df_population['Population'] = np.where(df_population['Population'].str.find("(") > 0, df_population['Population'].str.split(
+    r"(", expand=True)[0].str.replace(".", "", regex=True), df_population['Population'])
 df_population = df_population[["ID_IBGE", "Population"]].reset_index(drop=True)
 df_population = df_population.astype({"ID_IBGE": int, "Population": int})
 
-df_brazil = pd.DataFrame()
 
-for uf in states.id.unique():
-    url = (
-        "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
-        + str(uf)
-        + "/municipios"
-    )
-    r = requests.get(url)
-    states = json_normalize(data=r.json(), sep="")
-    df_brazil = df_brazil.append(states, ignore_index=True)
+async def get_async(url):
+    async with httpx.AsyncClient() as client:
+        return await client.get(url)
 
-df_brazil = df_brazil[
-    [
-        "id",
-        "microrregiaomesorregiaoUFregiaonome",
-        "microrregiaomesorregiaoUFsigla",
-        "microrregiaomesorregiaoUFnome",
-        "microrregiaonome",
-        "microrregiaomesorregiaonome",
-        "nome",
-    ]
-]
-df_brazil.rename(
-    columns={
-        "id": "ID_IBGE",
-        "nome": "City",
-        "microrregiaonome": "Microregion",
-        "microrregiaomesorregiaonome": "Mesoregion",
-        "microrregiaomesorregiaoUFsigla": "UF",
-        "microrregiaomesorregiaoUFnome": "State",
-        "microrregiaomesorregiaoUFregiaonome": "Region",
-    },
-    inplace=True,
-)
+urls = ["https://servicodados.ibge.gov.br/api/v1/localidades/estados"]
 
-df_brazil["find_latlon"] = (
-    df_brazil["City"].map(str)
-    + ", "
-    + df_brazil["UF"].map(str)
-    + ", "
-    + df_brazil["State"].map(str)
-    + ", Brazil"
-)
 
-df_brazil = pd.merge(df_brazil, df_population, on=["ID_IBGE"], how="left")
+async def obter_estados():
+    resps = await asyncio.gather(*map(get_async, urls))
+    data_json = [resp.json() for resp in resps]
 
-# df_brazil["proc_latlon"] = df_brazil["proc_latlon"].apply(geolocator)
-df_brazil["find_latlon"] = df_brazil["find_latlon"].progress_apply(geolocator)
-df_brazil["Latitude"] = df_brazil["find_latlon"].apply(
-    lambda x: x.latitude if x != None else None
-)
-df_brazil["Longitude"] = df_brazil["find_latlon"].apply(
-    lambda x: x.longitude if x != None else None
-)
-df_brazil = df_brazil[
-    [
-        "ID_IBGE",
-        "Region",
-        "UF",
-        "State",
-        "Microregion",
-        "Mesoregion",
-        "City",
-        "Latitude",
-        "Longitude",
-        "Population",
-    ]
-]
+    for html in data_json:
+
+        states = json_normalize(data=html, sep="")
+        list_urls = ["https://servicodados.ibge.gov.br/api/v1/localidades/estados/{}/municipios".format(
+            uf) for uf in states.id.unique()]
+
+    return list_urls
+
+
+async def launch2(urls):
+
+    resps = await asyncio.gather(*map(get_async, urls))
+    data_json = [resp.json() for resp in resps]
+    global df_cities
+    df_cities = pd.DataFrame()
+
+    for html in data_json:
+
+        states = json_normalize(data=html, sep="")
+        df_cities = df_cities.append(states, ignore_index=True)
+
+    df_cities = df_cities[["id", "microrregiaomesorregiaoUFregiaonome", "microrregiaomesorregiaoUFsigla",
+                           "microrregiaomesorregiaoUFnome", "microrregiaonome", "microrregiaomesorregiaonome", "nome"]]
+    df_cities.rename(columns={"id": "ID_IBGE", "nome": "City", "microrregiaonome": "Microregion", "microrregiaomesorregiaonome": "Mesoregion",
+                     "microrregiaomesorregiaoUFsigla": "UF", "microrregiaomesorregiaoUFnome": "State", "microrregiaomesorregiaoUFregiaonome": "Region"}, inplace=True)
+    df_cities['Location'] = df_cities['City'] + ", " + \
+        df_cities['UF'] + ", " + df_cities['State'] + ", Brazil"
+    return df_cities
+
+estados = asyncio.run(obter_estados())
+asyncio.run(launch2(estados))
+
+geolocator = Nominatim(user_agent="user_agent")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+location = []
+geopy_latitude = []
+geopy_longitude = []
+
+list_location = df_cities['Location'].head(5).to_list()
+
+for loc in tqdm(list_location):
+
+    time.sleep(1)
+    find_location = geocode(loc)
+    location.append(loc)
+    geopy_latitude.append(find_location.latitude)
+    geopy_longitude.append(find_location.longitude)
+
+colunas = ["Location", "Latitude", "Longitude"]
+df_location = pd.DataFrame(
+    zip(location, geopy_latitude, geopy_longitude), columns=colunas)
+
+df_cities_population = pd.merge(
+    df_cities, df_population, on=["ID_IBGE"], how="left")
+df_brazil = pd.merge(df_cities_population, df_location,
+                     on=["Location"], how="left")
+
+df_brazil = df_brazil[["ID_IBGE", "Region", "UF", "State", "Microregion",
+                       "Mesoregion", "City", "Latitude", "Longitude", "Population"]]
 df_brazil = df_brazil.set_index("ID_IBGE")
 df_brazil.to_csv("list_braziliancities.csv")
